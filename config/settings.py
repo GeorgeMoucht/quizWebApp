ae4f669186685f3b2629e5704253ebaeae73aac0
GeorgeMoucht <giorgosmoucht@gmail.com>
2024-12-22 18:57:51 +0200

    Rename 'quizWebApp' directory to 'config'.

--- config/settings.py
+++ config/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'quizWebApp.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -71,7 +71,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'quizWebApp.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
