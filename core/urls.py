629506d0a54c7ad3243151cc963bd1f23ed09495
symetsak <tsakanias.simos@gmail.com>
2025-01-24 16:05:56 +0200

    Create log in & register UI and fix hrefs for register and log out in navigation bar

--- core/urls.py
+++ core/urls.py
@@ -1,9 +1,8 @@
 #authentication/urls.py
-from django.urls import path
+from django.urls import path,include
 from . import views
 
 urlpatterns = [
     path('', views.homepage_view, name='homepage'),
+    path('authenticate/', include('authentication.urls')),
 ]
-
-#TestCommit
