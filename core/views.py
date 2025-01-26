c8d56ab05c84900b059ecb90f8a5e42033c24cad
symetsak <tsakanias.simos@gmail.com>
2025-01-26 13:34:02 +0200

    Teachers on Nav bar

--- core/views.py
+++ core/views.py
@@ -14,4 +14,20 @@ def homepage_view(request):
         return HttpResponse(
             "Template not found",
             status=404
-        )  
+        )
+    
+from django.shortcuts import render
+from django.contrib.auth.models import User, Group
+
+def teachers_view(request):
+    """
+    View for displaying the list of teachers.
+
+    This view fetches all users that belong to the 'teachers' group
+    and renders them in the 'teachers.html' template.
+    """
+    # Ανάκτηση του group των teachers
+    teachers_group = Group.objects.get(name="Teacher")
+    teachers = teachers_group.user_set.all()  # Παίρνουμε τους χρήστες του group
+
+    return render(request, 'core/teachers.html', {'teachers': teachers})
